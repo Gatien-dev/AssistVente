341c638a9b9b94c470056d919b3ab8c8eba37ced
Gatien GNAKOU-EDJAMBO <gatiengnakou@gmail.com>
2020-01-06 03:47:57 +0000

    Implementer la creation d'un achat

--- AssistVente/Controllers/AchatsController.cs
+++ AssistVente/Controllers/AchatsController.cs
@@ -1,4 +1,6 @@
 ﻿using AssistVente.Models;
+using AssistVente.Models.ViewModels;
+using Microsoft.AspNet.Identity;
 using System;
 using System.Collections.Generic;
 using System.Data.Entity;
@@ -44,9 +46,65 @@ namespace AssistVente.Controllers
             //}
         }
 
-        public ActionResult Details()
+        public ActionResult Create()
         {
-            return null;
+            var achatVM = new AchatCreateVM()
+            {
+                Details = new List<DetailAchatVM>()
+            };
+            var produits = db.Produits.ToList();
+            foreach (var produit in produits)
+            {
+                achatVM.Details.Add(new DetailAchatVM()
+                {
+                    NomProduit = produit.Nom,
+                    ProduitId = produit.ID,
+                    PU = produit.PrixAchat,
+                    Quantite = 0
+                });
+            }
+            return View(achatVM);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(AchatCreateVM achat)
+        {
+            if (ModelState.IsValid)
+            {
+                Achat newAchat = new Achat()
+                {
+                    Details = new List<DetailAchat>(),
+                    Fournisseur = achat.Fournisseur,
+                    Date = DateTime.Now,
+                    NumFacture=achat.NumFactureFournisseur,
+                    Id = Guid.NewGuid(),
+                    UserId = User.Identity.GetUserId()
+                };
+                double total = 0;
+                foreach (var detail in achat.Details)
+                {
+                    if (detail.Quantite > 0)
+                        newAchat.Details.Add(new DetailAchat()
+                        {
+                            Produit = db.Produits.Find(detail.ProduitId),
+                            QuantiteAchetee = detail.Quantite,
+                            ProduitID = detail.ProduitId,
+                            ID = Guid.NewGuid()
+                        });
+                    total += detail.Quantite * db.Produits.Find(detail.ProduitId).PrixAchat;
+                }
+                newAchat.Montant = total;
+                //newAchat.MontantRestant = total;
+
+                db.Operations.Add(newAchat);
+                db.SaveChanges();
+                //new CaisseManager(db).reglerVente(vente.MontantPaye, newVente);
+
+                return RedirectToAction("Index");
+            }
+            
+            return View(achat);
         }
     }
 }
---
+++ AssistVente/Models/ViewModels/stockLogIndexVM.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace AssistVente.Models.ViewModels
+{
+    public class stockLogIndexVM
+    {
+        public Guid Id { get; set; }
+        public Guid ProduitId { get; set; }
+        [Display(Name ="Produit")]
+        public string ProduitName { get; set; }
+        public OperationType Type { get; set; }
+        [Display(Name ="Quantité")]
+        public double Amount { get; set; }
+        public DateTime Date { get; set; }
+
+    }
+}
